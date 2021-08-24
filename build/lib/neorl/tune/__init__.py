9e71c776d104ec27fcee1107d892cc4b8a380786
MajdiRadaideh <mradaideh91@gmail.com>
2021-08-23 20:20:58 -0500

    1.5.5b: fixed a bug in terminal API for RL continue mode

--- build/lib/neorl/tune/__init__.py
+++
@@ -1,4 +0,0 @@
-from neorl.tune.gridtune import GRIDTUNE
-from neorl.tune.bayestune import BAYESTUNE
-from neorl.tune.estune import ESTUNE
-from neorl.tune.randtune import RANDTUNE
--- build/lib/neorl/tune/runners/__init__.py
+++
@@ -1,1 +0,0 @@
-#The tune files in this directory are special for the old NEORL that is compatible with textfile parser.
