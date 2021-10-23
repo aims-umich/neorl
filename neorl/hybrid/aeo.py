cb7781e6565a47972e135fb798f8226e4a44ab95
Dean Price <dean1357price1357@gmail.com>
2021-10-23 19:54:12 -0400

    set up file for developing aeo algorithm

---
+++ neorl/hybrid/aeo.py
@@ -0,0 +1,22 @@
+#    This file is part of NEORL.
+
+#    Copyright (c) 2021 Exelon Corporation and MIT Nuclear Science and Engineering
+#    NEORL is free software: you can redistribute it and/or modify
+#    it under the terms of the MIT LICENSE
+
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+
+class Population:
+    # Class to store information and functionality related to a single population
+    # in the AEO algorithm. Should be characterized by an evolution strategy passed
+    # as one of the optimization classes from the other algorithms in NEORL.
+    def __init__(self, strategy, pop_attr, conv = None):
+        print("we are running this")
+        pass
+
